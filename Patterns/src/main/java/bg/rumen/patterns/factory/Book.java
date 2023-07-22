f58922936789f1fedcd9990ae2ecd63e1f46222a
RlSk2112 <lrm1201@abv.bg>
2023-07-22 17:14:57 +0300

    Added simple example for factory.

---
+++ Patterns/src/main/java/bg/rumen/patterns/factory/Book.java
@@ -0,0 +1,57 @@
+package bg.rumen.patterns.factory;
+
+public class Book {
+
+    private String title;
+
+    private String author;
+
+    private Double price;
+
+    private Integer pages;
+
+
+    public Book() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Title -> %s%nAuthor -> %s%nPrice -> $%.2f%nPages -> %d",
+                this.title,
+                this.author,
+                this.price,
+                this.pages);
+    }
+}
---
+++ Patterns/src/main/java/bg/rumen/patterns/factory/Constants.java
@@ -0,0 +1,16 @@
+package bg.rumen.patterns.factory;
+
+public class Constants {
+
+    public static final String GOT_TITLE = "Game Of Thrones";
+    public static final String GOT_AUTHOR = "George R. R. Martin";
+    public static final Double GOT_PRICE = 25.22;
+    public static final Integer GOT_PAGES = 700;
+
+
+    public static final String HARRY_TITLE = "Harry Potter";
+    public static final String HARRY_AUTHOR = "J. K. Rowling";
+    public static final Double HARRY_PRICE = 15.00;
+    public static final Integer HARRY_PAGES = 766;
+
+}
